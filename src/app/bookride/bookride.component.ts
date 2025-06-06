97bfc71ed1f196ca527062ad38ba64861f4358e9
VIJAY-ATHITHYAA <g.v.vijayathithya@gmail.com>
2025-06-06 12:39:36 +0530

    started bookride component

---
+++ src/app/bookride/bookride.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bookride',
+  imports: [],
+  templateUrl: './bookride.component.html',
+  styleUrl: './bookride.component.css'
+})
+export class BookrideComponent {
+
+}
